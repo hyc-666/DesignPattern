46188ba34bdd72b64f28674da448f7e17a726039
hyc_666 <2213671872@qq.com>
2020-02-22 20:20:12 +0800

    里氏替换原则

--- DesignPattern/src/Principle/Liskov/Liskov.java
+++ DesignPattern/src/Principle/Liskov/Liskov.java
@@ -1,7 +1,7 @@
-package Liskov;
+package Principle.Liskov;
 //里氏替换原则
 /**
- * 1)里氏替 换原则(Liskov Substitution Principle)在1988年，由麻省理工学院的以为姓里的女士提出的。
+ * 1)里氏替 换原则(Principle.Liskov Substitution Principle)在1988年，由麻省理工学院的以为姓里的女士提出的。
  * 2)如果对每个类型为 T1的对象o1，都有类型为T2的对象o2，使得以T1定义的所有程序P在所有的对象o1都代换成o2时，
  *   程序P的行为没有发生变化，那么类型T2是类型T1的子类型。换句话说，
  *   所有引用基类的地方必须能透明地使用其子类的对象。
@@ -22,7 +22,7 @@ class A extends Base{
 class B extends Base{
     //如果想在B类里使用A类，则可以使用组合的方式
     //增加一个成员变量
-    private A a;
+    private A a = new A();
     public int fun1(int n1,int n2){
         return n1 + n2;
     }
